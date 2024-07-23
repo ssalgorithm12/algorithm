8e3862c39db3ed37036582c9bc131fff4a109a38
장보광 <154445682+JangBogwang@users.noreply.github.com>
2024-07-23 14:02:03 +0900

    [20934]_방울 마술_장보광

--- week1/[20934]_방울 마술_장보광.py
+++ week1/[20934]_방울 마술_장보광.py
@@ -1,10 +1,10 @@
 T = int(input())
 for t in range(1, T+1):
-    str = input().split()
-    a= [i for i in range(3) if str[0][i]=='o']
-    n = int(str[1])
+    s = input().split()
+    a= [i for i in range(3) if s[0][i]=='o'][0]
+    n = int(s[1])
     r = 0
-    if a[0]==2 and n == 0: r = 2;
-    elif a[0]==1 and (n&1)==0 : r = 1;
-    elif a[0]!=1 and (n&1)==1 : r = 1;
+    if a==2 and n == 0: r = 2;
+    elif a==1 and n&1==0 : r = 1;
+    elif a!=1 and n&1==1 : r = 1;
     print(f"#{t} {r}")
