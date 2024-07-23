d8747faaca13e53c6a35ef594e68e02a54c9e7e9
장보광 <154445682+JangBogwang@users.noreply.github.com>
2024-07-23 17:54:57 +0900

    [20934]방울 마술_장보광

--- week1/[20934]방울 마술_장보광.java
+++ week1/[20934]방울 마술_장보광.java
@@ -8,20 +8,12 @@ class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-         
         for(int x = 1; x <= t; x++)
         {
-            int a=0;
-            float[] arr = new float[3];
+            int a=0, result=0;
             String str = br.readLine();
-            for(int i = 0; i<3; i++) {
-            	if(str.charAt(i)=='o') {
-            		arr[i] = 1;
-            		a = i;
-            	}
-            }
+            for(int i = 0; i<3; i++) if(str.charAt(i)=='o') a = i;
             int n = Integer.parseInt(str.substring(4));
-            int result = 0;
             if(a==2&&n == 0) result = 2;
             else if(a==1 &&(n&1)==0 ) result = 1;
             else if(a!=1 &&(n&1)==1 ) result = 1;
