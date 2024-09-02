0862c21d2e27264cd50f5c8ba2afe13d68b46367
wkdqhrhkd12 <wkdqhrhkd12@gmail.com>
2024-09-03 08:28:44 +0900

    코드 최적화

--- week7/BOJ13305_주유소/[13305]_주유소_장보광.java
+++ week7/BOJ13305_주유소/[13305]_주유소_장보광.java
@@ -5,28 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 메모리: 36360kb 시간: 364ms
+// 메모리: 35560kb 시간: 356ms
 
 public class BOJ13305_주유소 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		long[] dist = new long[n]; //거리를 저장하는 배열
-		long[] price = new long[n]; // 가격을 저장하는 배열 
+		long price; // 가격을 저장하는 배열 
 		// 결과를 저장하는 변수 
 		long result = 0;
 		
-		// 거리&주유비 입력 
+		// 거리입력 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 1; i < n; i++) dist[i] = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < n-1; i++) dist[i] = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) price[i] = Integer.parseInt(st.nextToken());
-		long min = price[0];
-		
-		// 주유비 계산 
-		for(int i = 1; i < n; i++) {
+		long min = Integer.MAX_VALUE;
+		// 주유비 계산
+		for(int i = 0; i < n - 1; i++) {
+			price = Integer.parseInt(st.nextToken());
+			if(min > price) min = price;
 			result += (min * dist[i]);
-			if(min > price[i]) min = price[i];
 		}
 		
 		// 결과 출력 
