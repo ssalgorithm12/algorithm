39430d95c85f22bf7c7bee41eff95b2229d73d60
wkdqhrhkd12 <wkdqhrhkd12@gmail.com>
2024-09-04 08:38:46 +0900

    g

--- week7/BOJ_1004_어린왕자/[1004]_어린왕자_장보광.java
+++ week7/BOJ_1004_어린왕자/[1004]_어린왕자_장보광.java
@@ -1,26 +1,22 @@
 package algorithm;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-//메모리: 16080kb 시간: 152ms;
+import java.io.*;
+import java.util.*;
+//메모리: 15700kb 시간: 144ms;
 
 public class BOJ1004_어린왕자 {
 	public static void main(String[] args) throws IOException {
+		// 변수 및 입출력 선언 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		int[] x = new int[2];
 		int[] y = new int[2];
-		int[][] planet;
-		int count;
-		int[] check;
-		
+		int[] p = new int[3];
+	
 		for(int t = 1; t <= T; t++) {
-			count = 0;
+			int count = 0;
 			st = new StringTokenizer(br.readLine());
 			// 출발 지점과 도착 지점 입력
 			for(int i = 0; i < 2; i++) {
@@ -29,20 +25,19 @@ public class BOJ1004_어린왕자 {
 			}
 			
 			int n = Integer.parseInt(br.readLine());
-			planet = new int[n][3]; // 행성계 저장
+			// 각 행성계를 입력받는다. 
 			for(int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int j = 0; j < 3; j++) planet[i][j] = Integer.parseInt(st.nextToken());
+				int[] c = new int[2];
+				for(int j = 0; j < 3; j++) p[j] = Integer.parseInt(st.nextToken());
+				for(int j = 0; j < 2; j++) {
+					int a = (p[0] - x[j]);
+					int b = (p[1] - y[j]);	
+					if(a*a+b*b < p[2]*p[2]) c[j] = 1;
+				}
+				count+= (c[0] ^ c[1]);
 			}
 			
-			for(int i = 0; i < n; i++) {
-				check = new int[2];
-				for(int j = 0; j < 2; j++) {
-					int length = (planet[i][0] - x[j])*(planet[i][0] - x[j]) + (planet[i][1] - y[j])*(planet[i][1] - y[j]);
-					if(length < planet[i][2]*planet[i][2]) check[j] = 1;
-				}	
-				count+= (check[0] ^ check[1]);
-			}
 			sb.append(count+"\n");
 		}
 		System.out.print(sb);
