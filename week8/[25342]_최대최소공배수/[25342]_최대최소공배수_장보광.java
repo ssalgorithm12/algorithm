960f84e5b6c3a66dc89cce69467e4c76877bb44c
장보광 <154445682+JangBogwang@users.noreply.github.com>
2024-09-10 09:36:32 +0900

    [25342]_최대최소공배수_장보광(주석 추가)

--- week8/[25342]_최대최소공배수/[25342]_최대최소공배수_장보광.java
+++ week8/[25342]_최대최소공배수/[25342]_최대최소공배수_장보광.java
@@ -3,7 +3,7 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+// 메모리: 16136kb 시간: 132ms;
 public class BOJ25342_최대최소공배수 {
 	public static void main(String[] main) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,6 +13,8 @@ public class BOJ25342_최대최소공배수 {
 		for(int t = 0; t < T; t++) {
 			int n = Integer.parseInt(br.readLine());
 			int result = 1;
+			// n <= 7 일 떼 경우는 하드 코딩으로 모두 구현 
+			// 그 이상의 수 일 경우 짝수일 때와 홀수 일 때를 나눠서 구현
 			if(n==3) result = 6;
 			else if(n==4) result = 12;
 			else if(n==5) result = 60;
