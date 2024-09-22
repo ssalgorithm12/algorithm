21d4930b7e98262fa875471aed456523bdf90c5a
장보광 <154445682+JangBogwang@users.noreply.github.com>
2024-09-23 08:29:50 +0900

    [22342]_계산로봇_장보광

--- week10/[22342]_계산로봇/[22342]_계산로봇_장보광.java
+++ week10/[22342]_계산로봇/[22342]_계산로봇_장보광.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 메모리: 64356kb, 시간: 496ms
 public class BOJ22342_계산로봇 {
 	static int[] r = { -1, 0, 1};
 	public static void main(String []args) throws IOException {
