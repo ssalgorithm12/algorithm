90bef5eaabdaf20787d7273db6e423d6d614cb43
wkdqhrhkd12 <wkdqhrhkd12@gmail.com>
2024-10-18 08:41:19 +0900

    [28069]_김밥천국의계단

--- week13/[28069]_김밥천국의계단/[28069]_김밥천국의계단_장보광.java
+++ week13/[28069]_김밥천국의계단/[28069]_김밥천국의계단_장보광.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+//메모리: 18364kb, 시간: 140ms
 public class BOJ28069_김밥천국의계단 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,24 +10,18 @@ public class BOJ28069_김밥천국의계단 {
 		int k = Integer.parseInt(st.nextToken());
 		int[] dp = new int[n+1];
 		Arrays.fill(dp, Integer.MAX_VALUE);
-		if(n==1) {
-			dp[1] = 1;
-		}else {
-			dp[2] = 2;
-			
-			for(int i = 2; i < n; i++) {
-				if(dp[i+1] > dp[i]+1)
-					dp[i+1] = dp[i]+1;
-				int x = i + i/2;
-				if(x <= n && dp[x] > dp[i]+1)
-					dp[x] = dp[i]+1;
-			}
+		dp[1] = 1;
+					
+		for(int i = 1; i < n; i++) {
+			if(dp[i+1] > dp[i]+1)
+				dp[i+1] = dp[i]+1;
+			int x = i+i/2;
+			if(x <= n && dp[x] > dp[i]+1)
+				dp[x] = dp[i]+1;
 		}
+
 		
-		if(dp[n] <= k)
-			System.out.print("minigimbob");
-		else 
-			System.out.print("water");
-		
+		String r = dp[n]<=k? "minigimbob" : "water";
+		System.out.print(r);
 	}
 }
